086a779496d5dccb8955193d19d5c76da9018f50
Solomon Enanaw Kebede <101396246+solomonenanaw@users.noreply.github.com>
2023-02-03 20:53:50 +0300

    Update places_amenities.py
    all

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -1,63 +1,57 @@
-#!/usr/bin/python3
-"""Python file that works with api calls on Place and Amenity objects"""
+!/usr/bin/python3
+"""places_amenities.py"""
+import os
 from api.v1.views import app_views
-from flask import jsonify, request, abort
+from flask import abort, jsonify, make_response, request
 from models import storage
-from models.place import Place
 from models.amenity import Amenity
-from os import environ
+from models.place import Place
+from flasgger.utils import swag_from
 
 
-@app_views.route('/places/<place_id>/amenities', methods=['GET'])
-def place_amenities(place_id=None):
-    """Handles http requst without id for amenities linked with places"""
+@app_views.route('/places/<string:place_id>/amenities', methods=['GET'],
+                 strict_slashes=False)
+@swag_from('documentation/place_amenity/get_id.yml', methods=['GET'])
+def get_amenities(place_id):
+    """ retrieves all amenities from a place """
     place = storage.get(Place, place_id)
-
     if place is None:
         abort(404)
-    objs = storage.all('Amenity')
-    if environ.get('HBNB_TYPE_STORAGE') == 'db':
-        place_amenities = place.amenities
-    else:
-        place_amen_ids = place.amenities
-        place_amenities = []
-        for id in place_amen_ids:
-            place_amenities.append(storage.get(Amenity, id))
-    place_amenities = [
-        obj.to_dict() for obj in place_amenities
-        ]
-    return jsonify(place_amenities)
+    amenities = [obj.to_dict() for obj in place.amenities]
+    return jsonify(amenities)
 
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>',
-                 methods=['DELETE', 'POST'])
-def amenity_to_place(place_id=None, amenity_id=None):
-    """Handles http requests with id for amenties linked with places"""
+@app_views.route('/places/<string:place_id>/amenities/<string:amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
+@swag_from('documentation/place_amenity/delete.yml', methods=['DELETE'])
+def delete_amenity(place_id, amenity_id):
+    """ delete amenity from place """
     place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
     amenity = storage.get(Amenity, amenity_id)
-    if place is None:
-        abort(404)
     if amenity is None:
         abort(404)
+    if amenity not in place.amenities:
+        abort(404)
+    place.amenities.remove(amenity)
+    storage.save()
+    return jsonify({})
 
-    if request.method == 'DELETE':
-        if (amenity not in place.amenities and
-                amenity.id not in place.amenities):
-            abort(404)
-        if environ.get('HBNB_TYPE_STORAGE') == 'db':
-            place.amenities.remove(amenity)
-        else:
-            place.amenity_ids.pop(amenity.id, None)
-        place.save()
-        return jsonify({}), 200
 
-    if request.method == 'POST':
-        if (amenity in place.amenities or
-                amenity.id in place.amenities):
-            return jsonify(amenity.to_dict()), 200
-        if environ.get('HBNB_TYPE_STORAGE') == 'db':
-            place.amenities.append(amenity)
-        else:
-            place.amenities = amenity
-        place.save()
-        return jsonify(amenity.to_dict()), 201
+@app_views.route('/places/<string:place_id>/amenities/<string:amenity_id>',
+                 methods=['POST'], strict_slashes=False)
+@swag_from('documentation/place_amenity/post.yml', methods=['POST'])
+def post_amenity2(place_id, amenity_id):
+    """ post amenity by id """
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    if amenity in place.amenities:
+        return (jsonify(amenity.to_dict()), 200)
+    place.amenities.append(obj)
+    storage.save()
+    return (jsonify(amenity.to_dict(), 201))
