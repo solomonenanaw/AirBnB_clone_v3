9102175132d4f657a8e34f505b97623ace655d07
Solomon Enanaw Kebede <101396246+solomonenanaw@users.noreply.github.com>
2023-02-19 05:10:30 +0300

    Update index.py
    add

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,29 +1,37 @@
 #!/usr/bin/python3
-"""
-This module contains endpoint(route) status
-"""
-from models import storage
-from flask import Flask
+"""Python file that routes the Flask API's"""
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, request
+from models import storage
+
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
+from models.state import State
 
 
-@app_views.route('/status', strict_slashes=False)
-def status():
-    """
-    Returns a JSON status
-    """
+@app_views.route('/status', methods=['GET'])
+def show_status():
+    """An end point to retrive ok status as a response"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', strict_slashes=False)
-def count():
-    """
-    Retrieves the number of each objects by type
-    """
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """An endpoint to retrive the number of each objects by type"""
+    mapper = {
+            "amenities": Amenity,
+            "cities": City,
+            "places": Place,
+            "reviews": Review,
+            "states": State,
+            "users": User
+            }
+
+    result = {}
+    for k, v in mapper.items():
+        result[k] = storage.count(v)
+
+    return jsonify(result)
