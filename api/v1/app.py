b08a21aeb352c61334f9d71b7549b1840592eb61
Solomon Enanaw Kebede <101396246+solomonenanaw@users.noreply.github.com>
2023-02-19 05:06:34 +0300

    Update app.py
    CORS resolved

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,43 +1,41 @@
 #!/usr/bin/python3
-"""
-This module contains the principal application
-"""
+"""This module initiates the api"""
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, make_response, jsonify
 from os import getenv
 from flask_cors import CORS
-from flasgger import Swagger
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
-
-@app.teardown_appcontext
-def close_db(obj):
-    """ calls methods close() """
-    storage.close()
+cors = CORS(app, resources={r'/*': {'origins': '0.0.0.0'}})
 
 
 @app.errorhandler(404)
-def page_not_foun(error):
-    """ Loads a custom 404 page not found """
-    return make_response(jsonify({"error": "Not found"}), 404)
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-app.config['SWAGGER'] = {
-    'title': 'AirBnB clone - RESTful API',
-    'description': 'This is the api that was created for the hbnb restful api project,\
-    all the documentation will be shown below',
-    'uiversion': 3}
+@app.errorhandler(400)
+def bad_request(error):
+    d = error.description
+    msgs = ["Missing name", "Missing email",
+            "Missing password", "Missing user_id",
+            "Missing text"]
+    message = 'Not a Json' if d not in msgs else d
+    return make_response(jsonify({'error': message}), 400)
 
-Swagger(app)
+
+@app.teardown_appcontext
+def tear_down_db(execute):
+    """Removes the current SQLAlchemy session after each request
+    is completed"""
+    storage.close()
+
 
 if __name__ == "__main__":
-
-    host = getenv('HBNB_API_HOST', default='0.0.0.0')
-    port = getenv('HBNB_API_PORT', default=5000)
-
-    app.run(host, int(port), threaded=True)
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=getenv('HBNB_API_PORT', 5000),
+            threaded=True, debug=True)
